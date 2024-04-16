b524819d5bd5c7dd7bcf994cd39d935330a6c3c4
Sero Lee <sero.developer@gmail.com>
2024-04-16 19:36:12 +0900

    config.ini 파일 인코딩 ANSI로 하도록 수정

--- UntiyGameLogger/Config/BaseConfig.cs
+++ UntiyGameLogger/Config/BaseConfig.cs
@@ -8,9 +8,9 @@ namespace UnityGameLogger.Config
 	internal abstract class BaseConfig
 	{
 		#region kernel32.dll
-		[DllImport("kernel32")]
+		[DllImport("kernel32", CharSet = CharSet.Ansi)]
 		private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
-		[DllImport("kernel32")]
+		[DllImport("kernel32", CharSet = CharSet.Ansi)]
 		private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
 		#endregion
 
